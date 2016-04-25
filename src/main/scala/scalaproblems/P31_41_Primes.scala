28d51efdbd242e84b850db50550517f8dad7f41e
BorizZebr <borizzebr@gmail.com>
2016-04-25 23:42:17 +0700

    All problems from Arithmetic section

--- src/main/scala/scalaproblems/P31_41_Primes.scala
+++ src/main/scala/scalaproblems/P31_41_Primes.scala
@@ -7,12 +7,7 @@ import TestRuns._
 /**
   * Created by borisbondarenko on 24.04.16.
   */
-object P31_38_Primes extends App {
-
-  def isPrime(a: Int): Boolean =
-    primes takeWhile {_ <= sqrt(a)} forall { a % _ != 0}
-
-  val primes = Stream.cons(2, Stream.from(3, 2) filter { isPrime })
+object P31_41_Primes extends App {
 
   def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
@@ -21,11 +16,15 @@ object P31_38_Primes extends App {
     gcd(a, b) == 1
 
   implicit class IntImprovements(i: Int) {
+
+    def isPrime: Boolean =
+      primes takeWhile {_ <= sqrt(i)} forall { i % _ != 0}
+
     def totientCount : Int =
       (1 to i).count(isCoprime(_, i))
 
     def primeFactors : List[Int] = {
-      def loop(acc: List[Int], a: Int, p: Stream[Int]) : List[Int] = isPrime(a) match {
+      def loop(acc: List[Int], a: Int, p: Stream[Int]) : List[Int] = a.isPrime match {
         case true => a :: acc
         case false =>
           a % p.head == 0 match {
@@ -43,10 +42,24 @@ object P31_38_Primes extends App {
       i.primeFactorMultiplicity.foldLeft(1){ (a, b) =>
         b match { case (k, v) => a * (k - 1) * Math.pow (k, v - 1).toInt }
       }
+
+    def goldbach : (Int, Int) =
+      primes takeWhile { _ < i } find { a => (i - a).isPrime } flatMap { x => Some(x, i -x) } get
+
   }
 
+  val primes = Stream.cons(2, Stream.from(3, 2) filter { _ isPrime })
 
-  println(isPrime(7))
+
+  def listPrimesinRange(r: Range) : List[Int] =
+    primes dropWhile { _ < r.head } takeWhile { _ <= r.last } toList
+
+  def printGoldbachList(r: Range) : List[(Int, Int)] = printGoldbachListLimit(r, 0)
+
+  def printGoldbachListLimit(r: Range, l: Int) : List[(Int, Int)] =
+    r filter{ n => n > 2 && n % 2 == 0} map { _ goldbach } filter { _._1 >= l } toList
+
+  println(7 isPrime)
   println(gcd(120, 48))
   println(gcd(36, 63))
   println(315 primeFactors)
@@ -57,4 +70,9 @@ object P31_38_Primes extends App {
 
   time("Simple totient", 10090 totientCount)
   time("Improved totient", 10090 totientCountImproved)
+
+  println(listPrimesinRange(7 to 31))
+
+  println(34536 goldbach)
+  println(printGoldbachListLimit(1 to 2000, 50))
 }
