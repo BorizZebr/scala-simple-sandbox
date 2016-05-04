3bc962a3cc77d567174d8e9426e84430effc61a2
Bondarenko, Boris (Agoda) <boris.bondarenko@agoda.com>
2016-05-04 09:22:01 +0700

    Some tasks from bintrees section

--- src/main/scala/scalaproblems/P55_BinTrees.scala
+++ src/main/scala/scalaproblems/P55_BinTrees.scala
@@ -5,14 +5,40 @@ package scalaproblems
   */
 object P55_BinTrees extends App {
 
-  sealed abstract class Tree[+T]
+  sealed abstract class Tree[+T] {
+
+    def addValue[U >: T](x: U)(implicit ord: U => Ordered[U]): Tree[U]
+
+    def isSymmetric: Boolean
+
+    def isMirrorOf[A](t: Tree[A]): Boolean
+  }
 
   case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
-    override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+    override def toString =
+      "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+
+    override def isSymmetric: Boolean = left.isMirrorOf(right)
+
+    override def isMirrorOf[A](t: Tree[A]): Boolean = t match {
+      case Node(_, nl, nr) => nl.isMirrorOf(right) && nr.isMirrorOf(left)
+      case _ => false
+    }
+
+    override def addValue[U >: T](x: U)(implicit ord: (U) => Ordered[U]): Tree[U] =
+      if (x > value) Node(value, left, right.addValue(x))
+      else Node(value, left.addValue(x), right)
   }
 
   case object End extends Tree[Nothing] {
     override def toString = "."
+
+    override def isSymmetric: Boolean = true
+
+    override def isMirrorOf[A](t: Tree[A]): Boolean = t == End
+
+    override def addValue[U >: Nothing](x: U)(implicit ordered: U => Ordered[U]): Tree[U] =
+      Node(x)
   }
 
   object Node {
@@ -20,9 +46,22 @@ object P55_BinTrees extends App {
   }
 
   object Tree {
-    def cBalanced[T](count: Int, el: T): List[Node[T]] = count match {
-      
+    def cBalanced[T](count: Int, el: T): List[Tree[T]] = count match {
+      case n if n == 0 => List(End)
+
+      case n if n % 2 == 0 =>
+        val a = cBalanced((n - 1) / 2, el)
+        val b = cBalanced((n - 1) / 2 + 1, el)
+        a.flatMap(l => b.flatMap(g => List(Node(el, l, g), Node(el, g, l))))
+
+      case n if n % 2 == 1 =>
+        val a = cBalanced(n / 2, el)
+        a.flatMap(l => a.map(r => Node(el, l, r)))
     }
   }
 
+
+  println(Tree.cBalanced(4, "x"))
+
+  println(Node("a", Node("b"), Node("c")).isSymmetric)
 }
