5248dd5a88739a3d2071b23ac5bac43c9dbfde81
borizzebr <borizzebr@gmail.com>
2016-05-05 14:17:03 +0700

    --

--- src/main/scala/scalaproblems/P55_BinTrees.scala
+++ src/main/scala/scalaproblems/P55_BinTrees.scala
@@ -1,17 +1,33 @@
 package scalaproblems
 
+import java.lang.Math.log
+
 /**
   * Created by borisbondarenko on 03.05.16.
   */
 object P55_BinTrees extends App {
 
   sealed abstract class Tree[+T] {
-
     def addValue[U >: T](x: U)(implicit ord: U => Ordered[U]): Tree[U]
-
     def isSymmetric: Boolean
-
     def isMirrorOf[A](t: Tree[A]): Boolean
+    def count: Int
+    def leafCount: Int
+    def leafList: List[T]
+    def internalList: List[T]
+    def atLevel(level: Int): List[T]
+  }
+
+  case object End extends Tree[Nothing] {
+    override def toString = "."
+    override def isSymmetric: Boolean = true
+    override def isMirrorOf[A](t: Tree[A]): Boolean = t == End
+    override def addValue[U >: Nothing](x: U)(implicit ordered: U => Ordered[U]): Tree[U] = Node(x)
+    override def count: Int = 0
+    override def leafCount: Int = 0
+    override def leafList: List[Nothing] = Nil
+    override def internalList: List[Nothing] = Nil
+    override def atLevel(level: Int): List[Nothing] = Nil
   }
 
   case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
@@ -28,17 +44,29 @@ object P55_BinTrees extends App {
     override def addValue[U >: T](x: U)(implicit ord: (U) => Ordered[U]): Tree[U] =
       if (x > value) Node(value, left, right.addValue(x))
       else Node(value, left.addValue(x), right)
-  }
 
-  case object End extends Tree[Nothing] {
-    override def toString = "."
+    override def count: Int = left.count + right.count + 1
 
-    override def isSymmetric: Boolean = true
+    override def leafCount: Int = (left, right) match {
+      case (End, End) => 1
+      case (l, r) => l.leafCount + r.leafCount
+    }
 
-    override def isMirrorOf[A](t: Tree[A]): Boolean = t == End
+    override def leafList: List[T] = (left, right) match {
+      case (End, End) => List(value)
+      case (l, r) => left.leafList ::: right.leafList
+    }
 
-    override def addValue[U >: Nothing](x: U)(implicit ordered: U => Ordered[U]): Tree[U] =
-      Node(x)
+    override def internalList: List[T] = (left, right) match {
+      case (End, End) => Nil
+      case (l, r) => value :: left.internalList ::: right.internalList
+    }
+
+    override def atLevel(level: Int): List[T] = level match {
+      case 0 => Nil
+      case 1 => List(value)
+      case n => left.atLevel(n - 1) ::: right.atLevel(n - 1)
+    }
   }
 
   object Node {
@@ -47,7 +75,7 @@ object P55_BinTrees extends App {
 
   object Tree {
     def cBalanced[T](count: Int, el: T): List[Tree[T]] = count match {
-      case n if n == 0 => List(End)
+      case 0 => List(End)
 
       case n if n % 2 == 0 =>
         val a = cBalanced((n - 1) / 2, el)
@@ -65,21 +93,54 @@ object P55_BinTrees extends App {
     def fromList[T](list: List[T])(implicit ord: T => Ordered[T]): Tree[T] =
       list.foldLeft(End: Tree[T]) { (a, b) => a.addValue(b) }
 
+    def hBalancedWithHeight[T](height: Int, el: T): List[Tree[T]] = height match {
+      case 1 => List(Node(el))
+      case n if n < 1 => List(End)
+      case n =>
+        val long = hBalancedWithHeight(n - 1, el)
+        val short = hBalancedWithHeight(n - 2, el)
+        long.flatMap(a => long.map(b => Node(el, a, b))) :::
+          long.flatMap(l => short.flatMap(s => List(Node(el, l, s), Node(el, s, l))))
+    }
+
     def minHBalNodes(height: Int): Int = height match {
       case h if h <= 0 => 0
       case 1           => 1
       case h           => 1 + minHBalNodes(h - 1) + minHBalNodes(h - 2)
     }
 
-    
+    def minHBalHeight(nodes: Int): Int =
+      if (nodes == 0) 0
+      else (log(nodes) / log(2)).toInt + 1
+
+    def maxHbalHeight(nodes: Int): Int =
+      if (nodes == 0) 0
+      else nodes / 2 + 1
+
+    def hBalanced[T](nodes: Int, value: T): List[Tree[T]] =
+      (for{
+        h <- minHBalHeight(nodes) to maxHbalHeight(nodes)
+        tree <- hBalancedWithHeight(h, value).filter(_.count == nodes)
+      } yield tree).toList
+
+    def completeBinaryTree[T](nodes: Int, value: T): Tree[T] = nodes match {
+      case 0 => End
+      case 1 => Node(value)
+      case n => Node(value, completeBinaryTree(nodes/2, value), completeBinaryTree((nodes - 1)/2, value))
+    }
+
   }
 
 
-  println(Tree.cBalanced(4, "x"))
+  println(Tree.cBalanced(5, "x"))
 
   println(Node("a", Node("b"), Node("c")).isSymmetric)
 
   println(End.addValue("a"))
 
   println(Tree.fromList(List(3, 2, 1, 5, 8, 2, 10)))
+
+  println(Tree.hBalanced(4, "x"))
+
+  println(Tree.completeBinaryTree(5, "x"))
 }
